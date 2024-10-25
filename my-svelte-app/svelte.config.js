23eeae704aca82095317917cd55e6ee2452d5e9d
lewdry <30194191+lewdry@users.noreply.github.com>
2024-10-26 00:42:40 +1100

    Refactor svelte.config.js to prevent hashing of asset files

--- my-svelte-app/svelte.config.js
+++ my-svelte-app/svelte.config.js
@@ -1,7 +1,19 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    vite: {
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: '[name][extname]' // Prevent hashing of asset files
+          }
+        }
+      }
+    }
+  }
+};
